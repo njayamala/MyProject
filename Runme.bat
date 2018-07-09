c0020d00533a1e40b41ff24af56c9a8d54b6bd9e
Jayamala <Jayamala@Jayamala-PC>
2018-07-09 18:04:58 +0530

    second commit

---
+++ Runme.bat
@@ -0,0 +1,4 @@
+set projectLocation=D:\seleniumworkspace\ProjectFramework1
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\Jars\*
+java org.testng.TestNG %projectLocation%\testng.xml
